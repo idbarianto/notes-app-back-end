fcecc48649c2b596c9b33a97b78e582106d6d25b
Sugeng Arif Barianto <sugengarifbarianto@Barianto-MacBook-Pro.local>
2022-08-06 01:12:59 +0700

    feat: initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+module.exports = routes;
